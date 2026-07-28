1b804cfce0b6c2077251b9a2ac6ccae584c5f783
Lucas Arantes Camacho <lucas.camacho@alumni.usp.br>
2026-07-28 12:41:58 +0200

    create R

--- R/Scripts/tsuboi/Create_P.R
+++ R/Scripts/tsuboi/Create_P.R
@@ -1,2 +1,133 @@
-# CREATE P MATRICES FOR SAME DIM AS G SAGUINUS
+# CREATE P MATRICES WITH SAME DIM AS G SAGUINUS
+
+# load packages
+if(!require(evolqg)){install.packages("evolqg"); library(evolqg)}
+if(!require(openxlsx)){install.packages("openxlsx"); library(openxlsx)}
+
+AppendMe <- function(dfNames) {
+  do.call(rbind, lapply(dfNames, function(x) {
+    cbind(get(x), source = x)
+  }))
+}
+
+# Read and unique species
+msrs_catarrhini = read.csv(file = "~/Dropbox/Doc/Data/primates_measures/medidas_catarrhini.csv", 
+                           dec = ",", sep = ",")
+
+# Check catarrhini mrsr
+# check names and remove doubts
+msrs_catarrhini$SEX[which(msrs_catarrhini$SEX == "?female")] = "female"
+msrs_catarrhini$SEX[which(msrs_catarrhini$SEX == "?male")] = "male"
+
+# remove uncertain sex
+msrs_catarrhini$SEX[msrs_catarrhini$SEX == "0"] = NA
+msrs_catarrhini$SEX[msrs_catarrhini$SEX == ""] = NA
+msrs_catarrhini$SEX[msrs_catarrhini$SEX == "sexo"] = NA
+
+msrs_catarrhini = msrs_catarrhini[complete.cases(msrs_catarrhini$SEX), ]
+
+#
+msrs_platyrrhini = read.csv(file = "~/Dropbox/Doc/Data/primates_measures/medidas_platyrrhini.csv", 
+                            dec = ".", sep = ",")
+
+# Check platyrrhini msrs
+index = which(apply(msrs_platyrrhini[, 23:61], 1, 
+                    function(x) any(is.na(x) | (x == "" & !is.numeric(x)))))
+msrs_platyrrhini = msrs_platyrrhini[-index,]
+
+# remove uncertain sex
+msrs_platyrrhini$SEX4.[msrs_platyrrhini$SEX4. == ""] = NA
+msrs_platyrrhini$SEX4.[msrs_platyrrhini$SEX4. == " "] = NA
+
+msrs_platyrrhini = msrs_platyrrhini[complete.cases(msrs_platyrrhini$SEX4.), ]
+
+# Catarrhini
+for(i in 1:length(species)){
+  genus = strsplit(species[i], "_")[[1]][1]
+  sp = strsplit(species[i], "_")[[1]][2]
+  
+  species_subset = msrs_catarrhini[which(msrs_catarrhini$GENUS == genus), ]
+  species_subset = species_subset[which(species_subset$SPECIES == sp), ]
+  
+  if (nrow(species_subset) < 40) {
+    print(i)
+    next
+  }
+  
+  # separate M and F
+  sub_sexes_m = species_subset[which(species_subset$SEX == "male"), ]
+  sub_sexes_f = species_subset[which(species_subset$SEX == "female"), ]
+  
+  #if (nrow(sub_sexes_f) < 40 || nrow(sub_sexes_m) < 40) {
+  #  print(i)
+  #  next
+  #}
+  
+  #fit = lm(log(as.matrix(species_subset[,49:87])) ~ species_subset$SEX)
+  # REMOVE TRAITS IN ORDER TO HAVE THE SAME DIM AS G SAGUINUS
+  # REMOVE PTTSP, EAMZYGO E MAIS UM...
+  species_subset <- subset(species_subset, select = -PTTSP)
+  
+  fit = lm(log(as.matrix(species_subset[,49:86])) ~ species_subset$SEX)
+  cov.matrix = CalculateMatrix(fit)
+  
+  pasta_destino <- "~/Dropbox/Doc/Code/evowm/R/Outputs/log/" # mudar conforme necessario
+  arquivo <- paste0(pasta_destino, genus, "_", sp, ".csv")
+  
+  write.table(cov.matrix, file = arquivo, col.names = NA)
+}
+
+# Platyrrhini
+setwd("~/Dropbox/Doc/Data/p_vcv_gabriel/")
+temp = list.files(pattern = "*.csv")
+species  = gsub(".csv", replacement = "", temp)
+
+# 
+for(i in 1:length(species)){
+  genus = strsplit(species[i], "_")[[1]][1]
+  sp = strsplit(species[i], "_")[[1]][2]
+  
+  species_subset = msrs_platyrrhini[which(msrs_platyrrhini$GENUS. == genus), ]
+  species_subset = species_subset[which(species_subset$SPECIES. == sp), ]
+  
+  if (nrow(species_subset) < 40) {
+    print(i)
+    next
+  }
+  
+  # separate M and F
+  sub_sexes_m = species_subset[which(species_subset$SEX4. == "M"), ]
+  sub_sexes_f = species_subset[which(species_subset$SEX4. == "F"), ]
+  
+  #if (nrow(sub_sexes_f) < 20 || nrow(sub_sexes_m) < 20) {
+  #  print(i)
+  #  next
+  #}
+  species_subset <- subset(species_subset, select = -V20)
+  
+  fit = lm(log(as.matrix(species_subset[,23:60])) ~ species_subset$SEX4.)
+  cov.matrix = CalculateMatrix(fit)
+  
+  pasta_destino <- "~/Dropbox/Doc/Code/evowm/R/Outputs/log/" # mudar conforme necessario
+  arquivo <- paste0(pasta_destino, genus, "_", sp, ".csv")
+  
+  write.table(cov.matrix, file = arquivo, col.names = NA)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
--- R/Scripts/tsuboi/teste_tsuboi.R
+++ R/Scripts/tsuboi/teste_tsuboi.R
@@ -10,36 +10,43 @@ library(evolqg)
 library(ape)
 library(dplyr)
 library(slouch)
+library(phytools)
+library(parallel)
 
 # read all species VCV matrices
-setwd("~/Dropbox/Doc/Data/vcv/")
-temp <- list.files(pattern = "*.csv")
-vcv <- lapply(temp, read.csv, header = FALSE, dec = ",")
-names(vcv) <- gsub(".csv", replacement = "", temp)
-vcv$Lagothrix_lagothricha <- as.data.frame(lapply(vcv$Lagothrix_lagothricha, 
-                                                  function(x) 
-                                                    as.numeric(trimws(x))))
-vcv$Cacajao_calvus <- read.csv("~/Dropbox/Doc/Data/p_vcv_gabriel/Cacajao_calvus.csv", 
-                              header = FALSE, sep = ";", 
-                              dec = ",")
-vcv$Cacajao_calvus <- as.data.frame(lapply(vcv$Cacajao_calvus, 
-                                           function(x) 
-                                             as.numeric(trimws(x))))
+setwd("~/Dropbox/Doc/Code/evowm/R/Outputs/log/")
+temp = list.files(pattern = "*.csv")
+vcv = lapply(temp, read.csv, header = TRUE, dec = ".", sep = ' ', row.names = 1)
+names(vcv)  = gsub(".csv", replacement= "", temp)
+
 # read phylogeny
-mds <- readRDS("~/Dropbox/Doc/Code/evowm/R/Outputs/Haplorrhini_MDS_Matings.RDS")
 filename <- "~/Dropbox/Doc/Data/Primates_Dryad_no_scripts/median_tree.tre.nex"
 tree <- read.nexus(filename)
-tree$tip.label[tree$tip.label == "Lagothrix_lagotricha"] <- "Lagothrix_lagothricha"
-species <- mds$especies
+species <- names(vcv)
 tree <- drop.tip(tree, setdiff(tree$tip.label, species))
+
+# remove vcv which are not in the phylogeny
+vcv <- vcv[!names(vcv) %in% setdiff(names(vcv), tree$tip.label)]
+species <- names(vcv)
 
 # get ancestral VCV eigenvectors
 all_cov_matrices <- PhyloW(tree, vcv)
 ancestral <- getMRCA(tree, tree$tip.label)
-pcs <- eigen(all_cov_matrices$`66`)$vectors
+anc_vcv <- all_cov_matrices[[as.character(ancestral)]]
+
+# zero first eigenvalue
+eig <- eigen(anc_vcv)
+D <- diag(eig$values)
+V <- eig$vectors
+D2 <- D
+D2[1,1] <- min(eig$values[-1]) * 1e-8
+anc_vcv <- V %*% D2 %*% t(V)
+
+# get pcs
+pcs <- eigen(anc_vcv)$vectors
 
 # read G matrix
-g_m <- readRDS("~/Dropbox/Doc/Code/evowm/R/Scripts/tsuboi/Saguinus.rds")$G_cov
+#g_m <- readRDS("~/Dropbox/Doc/Code/evowm/R/Scripts/tsuboi/Saguinus.rds")#$G_cov
 
 # calculating e and c in orthogonal axis defined by ancestral P
 n <- length(species) * ncol(pcs)
@@ -58,11 +65,20 @@ k <- 1
 for(i in seq_along(species)){
   
   covar <- as.matrix(vcv[[species[i]]])
+  
+  # remove first eingenvalue or put almost zero on it
+  eig <- eigen(covar)
+  D <- diag(eig$values)
+  V <- eig$vectors
+  D2 <- D
+  D2[1,1] <- min(eig$values[-1]) * 1e-8
+  covar <- V %*% D2 %*% t(V)
+
+  #
   e_mean <- evolvabilityMeans(covar)[1]
   c_mean<- evolvabilityMeans(covar)[4]
   
   for(j in seq_len(ncol(pcs))){
-    
     evo <- evolvabilityBeta(covar, pcs[, j])
     
     results_e[k, ] <- list(
@@ -186,6 +202,41 @@ results_mean <- results_e %>%
     .groups = "drop"
   )
 
+# Estimate R matrix
+medias <- readRDS("~/Dropbox/Doc/Code/evowm/R/Scripts/evolvability/averages_PCS_autovalues_primates.RDS")
+
+species_means <- t(sapply(medias$ByTrait_Averages, function(sp) {
+  (sp$Machos + sp$Fêmeas) / 2
+}))
+
+species_means <- as.matrix(species_means)
+
+R <- evol.vcv(tree, species_means)
+
+# trait_sets <- list(
+#   traits_1_5   = 1:5,
+#   traits_6_10  = 6:10,
+#   traits_11_15 = 11:15,
+#   traits_16_20 = 16:20,
+#   traits_21_25 = 21:25,
+#   traits_26_30 = 26:30,
+#   traits_31_35 = 31:35,
+#   traits_36_38 = 36:38
+# )
+# 
+# n_cores <- max(1, detectCores() - 1)
+# 
+# R_list <- mclapply(
+#   trait_sets,
+#   function(idx) {
+#     evol.vcv(tree, species_means[, idx, drop = FALSE])
+#   },
+#   mc.cores = n_cores
+# )
+
+#saveRDS(R_list, "~/Dropbox/Doc/Code/evowm/R/Scripts/evolvability/R_list.RDS")
+saveRDS(R, "~/Dropbox/Doc/Code/evowm/R/Scripts/evolvability/R.RDS")
+
 # FIGURES
 # FIGURE 5
 plot(log(results_mean$Mean_Evolvability), results_g$GeodesicDistance)
@@ -199,7 +250,7 @@ df_ou <- data.frame(
 
 df_ou <- df_ou[match(tree$tip.label, df_ou$species), ]
 
-alpha_vals <- seq(0.01, 0.1, length.out = 20)
+alpha_vals <- seq(0.1, 0.5, length.out = 20)
 fit_ou_e_geodist <- slouch.fit(
   phy = tree,
   response = df_ou$mean_evolvability,
@@ -238,7 +289,7 @@ df_ou <- data.frame(
 
 df_ou <- df_ou[match(tree$tip.label, df_ou$species), ]
 
-alpha_vals <- seq(0.01, 0.1, length.out = 20)
+alpha_vals <- seq(0.1, 0.5, length.out = 20)
 fit_ou_eu_geo <- slouch.fit(
   phy = tree,
   response = df_ou$eudist,
@@ -248,17 +299,7 @@ fit_ou_eu_geo <- slouch.fit(
 )
 plot(fit_ou_eu_geo)
 
-
-
-
-
-
-
-
-
-
-
-
+# USING RAW TRAITS (MEANS) AS AXIS INSTEAD OF DO TRANSFORMATIONS
 
 
 
